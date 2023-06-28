d02e22c030636a10f5160083a17ca947d7229af0
Paulo Guilherme S. Reis <paulosantos--@live.com>
2023-06-28 16:50:30 -0400

    feat: Implementa funlçoes de calcular distancia total e gerar ordem aleatoria

---
+++ utils/order.py
@@ -0,0 +1,24 @@
+import random
+from typing import List
+import numpy as np
+from models.city import City
+
+
+def calculate_total_distance(
+    cities: List[City], order: List[int], distances: np.ndarray
+) -> float:
+    total_distance = 0
+    num_cities = len(cities)
+
+    for i in range(num_cities):
+        current_city = order[i]
+        next_city = order[(i + 1) % num_cities]
+        total_distance += distances[current_city, next_city]
+
+    return total_distance
+
+
+def generate_random_order(num_cities: int) -> List[int]:
+    order = list(range(num_cities))
+    random.shuffle(order)
+    return order
