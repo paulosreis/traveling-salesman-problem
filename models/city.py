a692066fd89b56eeaac897eba5d153235c40705a
Paulo Guilherme S. Reis <paulosantos--@live.com>
2023-06-28 16:49:15 -0400

    feat: Implementa classe city com metodo para calcucar distancia

---
+++ models/city.py
@@ -0,0 +1,13 @@
+import math
+
+
+class City:
+    def __init__(self, identifier: int, x: int, y: int):
+        self.identifier = identifier
+        self.x = x
+        self.y = y
+
+    def calculate_distance(self, other: "City") -> float:
+        x1, y1 = self.x, self.y
+        x2, y2 = other.x, other.y
+        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
